135668f868f1606c5576906d89d683c89405e179
Lucho Lesca <lescaluciano@hotmail.com>
2021-09-13 15:25:26 -0300

    Add cube, cube--moved and cube control

--- 10-faq-accordion-card-main/js/index.js
+++ 10-faq-accordion-card-main/js/index.js
@@ -1,7 +1,24 @@
-const $headers = document.querySelectorAll("#accordion-header")
+const $headers = document.querySelectorAll("#accordion-header") /*  */
+const $cube = document.getElementById("cube")
+let showed = 0
 
 $headers.forEach((header) => {
 	header.addEventListener("click", (e) => {
-		e.target.parentElement.classList.toggle("collapse--show")
+		// e.target.parentElement.classList.toggle("collapse--show")
+		if (e.target.parentElement.classList.contains("collapse--show")) {
+			e.target.parentElement.classList.remove("collapse--show")
+			showed--
+		} else {
+			e.target.parentElement.classList.add("collapse--show")
+			showed++
+		}
+
+		if (showed > 0) {
+			if (!$cube.classList.contains("cobe--moved")) {
+				$cube.classList.add("cube--moved")
+			}
+		} else {
+			$cube.classList.remove("cube--moved")
+		}
 	})
 })
