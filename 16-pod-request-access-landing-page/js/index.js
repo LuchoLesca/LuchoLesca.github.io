a98f39b7cbb484f146de241d093666a3d080b54f
Lucho Lesca <lescaluciano@hotmail.com>
2021-10-04 23:11:10 -0300

    Finish js. All finished

--- 16-pod-request-access-landing-page/js/index.js
+++ 16-pod-request-access-landing-page/js/index.js
@@ -1,25 +1,28 @@
 const emailRegex =
 	/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const emptyStringRegex = /^\s*$/
 
 const $form = document.getElementById("form")
 
+const evaluateErrorMsgForInputEmail = (inputValue) => {
+	if (emptyStringRegex.test(inputValue)) {
+		return "Oops! Please add your email"
+	}
+	if (!emailRegex.test(inputValue)) {
+		return "Oops! Please ckeck your email"
+	}
+	return ""
+}
+
 const handleSubmit = (e) => {
+	e.preventDefault()
+
 	const emailInputNode = [...$form.elements].find(
 		(element) => element.tagName === "INPUT" && element.name === "email"
 	)
-	const emailInputValue = emailInputNode.value.trim("")
-	if (emailInputValue === "") {
-		$form.dataset.errorMsg = "Oops! Please add your email"
-	} else {
-		if (!emailRegex.test(emailInputValue)) {
-			$form.dataset.errorMsg = "Oops! Please ckeck your email"
-		} else {
-			$form.dataset.errorMsg = ""
-		}
-	}
+	const emailInputValue = emailInputNode.value
+	const errorMsg = evaluateErrorMsgForInputEmail(emailInputValue)
+	$form.dataset.errorMsg = errorMsg
 }
 
-$form.addEventListener("submit", (e) => {
-	e.preventDefault()
-	handleSubmit(e)
-})
+$form.addEventListener("submit", handleSubmit)
